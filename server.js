16859609e0184d1e24fe84747cd1a7c54b4003d9
Renato Cordova <ren99cordova@gmail.com>
2022-11-28 16:12:33 -0500

    routes seem to be working, and new notes are being saved but not displayed on the page

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
 const express = require(`express`);
 const path = require(`path`);
-const api = require(`./routes/index.js`);
+const { readFromFile, readAndAppend } = require(`./helpers/fsUtils`);
+const uuid = require(`./helpers/uuid`);
 
 const PORT = 5500;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(`/api`, api);
 
 app.use(express.static(`public`));
 
-app.get(`/`, (req, res) => {
+//Routes
+app.get(`/notes`, (req, res) => {
+  console.log(`Note page requested`);
+  res.sendFile(path.join(__dirname, `/public/notes.html`));
+});
+
+app.get(`/*`, (req, res) => {
+  console.log(`Home page requested`);
   res.sendFile(path.join(__dirname, `/public/index.html`));
 });
 
-app.get(`/notes`, (req, res) => {
-  res.sendFile(path.join(__dirname, `/public/notes.html`));
+//APIs
+app.get(`/api/notes`, (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile(`./db/db.json`).then((data) => res.json(JSON.parse(data)));
+});
+
+app.post(`/api/notes`, (req, res) => {
+  console.info(`${req.method} request received to add new note`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, `./db/db.json`);
+
+    const response = {
+      status: `success`,
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json(`Error in saving new note`);
+  }
 });
 
 app.listen(PORT, () =>
